8e93a5d814e09de6183a05b2b6c8347fbddad224
BatiYounes <younesbati007@gmail.com>
2023-11-15 13:45:01 -0800

    fixing bugs

---
+++ File_7.c
@@ -0,0 +1,194 @@
+#include "main.h"
+
+/**
+ * exit_error - Generate an error message for shell exit.
+ * @sh: Shell structure.
+ *
+ * Return: Error message.
+ */
+char *exit_error(myshell *sh)
+{
+	char *error, *str = integer_to_string(sh->line_count);
+	int l = string_length(sh->av[0]) + string_length(str);
+
+	l += string_length(sh->args[0]) + string_length(sh->args[1]) + 23;
+	error = malloc(sizeof(char) * (l + 1));
+	if (error == 0)
+	{
+		free(str);
+			return (NULL);
+	}
+
+	copy_string(error, sh->av[0]);
+	concatenate_strings(error, ": ");
+	concatenate_strings(error, str);
+	concatenate_strings(error, ": ");
+	concatenate_strings(error, sh->args[0]);
+	concatenate_strings(error, ": Illegal number: ");
+	concatenate_strings(error, sh->args[1]);
+	concatenate_strings(error, "\n\0");
+	free(str);
+
+	return (error);
+}
+
+/**
+ * env_error - Generate an error message for environment variable error
+ * @sh: Shell structure.
+ *
+ * Return: Error message.
+ */
+char *env_error(myshell *sh)
+{
+	char *error;
+	char *s = integer_to_string(sh->line_count);
+	char *message = ": Unable to add or change or remove env\n";
+	int l;
+
+	l = string_length(sh->av[0]) + string_length(s);
+	l = l + string_length(sh->args[0]) + string_length(message) + 5;
+
+	error = malloc((l + 1) * sizeof(char));
+	if (error == 0)
+	{
+		free(error);
+		free(s);
+		return (NULL);
+	}
+
+	copy_string(error, sh->av[0]);
+	concatenate_strings(error, ": ");
+	concatenate_strings(error, s);
+	concatenate_strings(error, ": ");
+	concatenate_strings(error, sh->args[0]);
+	concatenate_strings(error, message);
+	concatenate_strings(error, "\0");
+	free(s);
+
+	return (error);
+}
+
+/**
+ * handle_errors - Handle and display errors.
+ * @sh: Shell structure.
+ * @error: Error code.
+ *
+ * Return: Error code.
+ */
+int handle_errors(myshell *sh, int error)
+{
+	char *err;
+	int check = compare_strings("cd", sh->args[0]);
+	int exit = compare_strings("exit", sh->args[0]);
+
+	if (error == 127)
+	{
+		err = not_found_error(sh);
+	}
+	if (error == 126)
+	{
+		err = path_error(sh);
+	}
+	if (error == -1)
+	{
+		err = env_error(sh);
+	}
+	if (error == 2)
+		{
+		if (check == 0)
+		{
+			err = cd_error(sh);
+		}
+		else if (exit == 0)
+		{
+			err = exit_error(sh);
+		}
+	}
+
+	if (err)
+	{
+		write(STDERR_FILENO, err, string_length(err));
+		free(err);
+	}
+
+	sh->status = error;
+
+	return (error);
+}
+
+/**
+ * command_error - Check for command execution errors.
+ * @ddir: Pointer to the destination directory.
+ * @sh: Shell structure.
+ *
+ * Return: 1 if there is an error, 0 if not.
+ */
+int command_error(char *ddir, myshell *sh)
+{
+	int i, j, k;
+
+	if (ddir == NULL)
+	{
+		handle_errors(sh, 127);
+			return (1);
+	}
+
+	i = compare_strings(sh->args[0], ddir);
+	if (i != 0)
+	{
+		j = access(ddir, X_OK);
+		if (j == -1)
+		{
+			handle_errors(sh, 126);
+			free(ddir);
+				return (1);
+		}
+		free(ddir);
+	}
+	else
+	{
+		k = access(sh->args[0], X_OK);
+		if (k == -1)
+		{
+			handle_errors(sh, 126);
+				return (1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * not_found_error - Generate an error message for command not found.
+ * @sh: Shell structure.
+ *
+ * Return: Error message.
+ */
+char *not_found_error(myshell *sh)
+{
+	char *error, *s = integer_to_string(sh->line_count);
+	int str_length;
+
+	str_length = string_length(sh->av[0]) + string_length(s);
+	str_length = str_length + string_length(sh->args[0]) + 18;
+
+	error = malloc((str_length + 1) * sizeof(char));
+	if (error == 0)
+	{
+		free(error);
+		free(s);
+		return (NULL);
+	}
+
+	copy_string(error, sh->av[0]);
+	concatenate_strings(error, ": ");
+	concatenate_strings(error, s);
+	concatenate_strings(error, ": ");
+
+	concatenate_strings(error, sh->args[0]);
+	concatenate_strings(error, ": No such file or directory\n");
+	concatenate_strings(error, "\0");
+	free(s);
+
+	return (error);
+}
---
+++ File_9.c
@@ -0,0 +1,155 @@
+#include "main.h"
+
+/**
+ * copy_memory - Copy memory from source to destination.
+ * @dest_ptr: Destination pointer.
+ * @src_ptr: Source pointer.
+ * @size: Size of the memory to copy.
+ *
+ * Return: Nothing.
+ */
+void copy_memory(void *dest_ptr, const void *src_ptr, unsigned int size)
+{
+	char *dest = (char *)dest_ptr;
+	char *src = (char *)src_ptr;
+
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+}
+/**
+ * reallocate_mem - Reallocate memory.
+ * @ptr: Pointer to the previously allocated memory.
+ * @old: Size, in bytes, of the old memory block.
+ * @nw: New size, in bytes, of the memory block.
+ *
+ * Return: If nw == old, returns ptr.
+ * If reallocation fails, returns NULL.
+ */
+void *reallocate_mem(void *ptr, unsigned int old, unsigned int nw)
+{
+	void *nwm_ptr;
+
+	if (ptr == NULL)
+	{
+		return (malloc(nw));
+	}
+	if (nw == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (nw == old)
+	{
+		return (ptr);
+	}
+	nwm_ptr = malloc(nw);
+	if (nwm_ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (nw < old)
+	{
+		copy_memory(nwm_ptr, ptr, nw);
+	}
+	else
+	{
+		copy_memory(nwm_ptr, ptr, old);
+	}
+	free(ptr);
+	return (nwm_ptr);
+}
+
+/**
+ * free_separator_list - Free a separator singly linked list.
+ * @start: Head of the linked list.
+ *
+ * Return: Nothing.
+ */
+void free_separator_list(myseparator **start)
+{
+	myseparator *current, *temp;
+
+	if (start != NULL)
+	{
+		current = *start;
+
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+
+		*start = NULL;
+	}
+}
+
+/**
+ * free_cline_list - Free a command line singly linked list.
+ * @start: Head of the linked list.
+ *
+ * Return: void, no return.
+ */
+void free_cline_list(mycline **start)
+{
+	mycline *current, *temp;
+
+	if (start != NULL)
+	{
+		current = *start;
+
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+
+		*start = NULL;
+	}
+}
+
+/**
+ * reallocate_dp - Reallocate memory for a double pointer.
+ * @old_dp: Double pointer to the previously allocated memory.
+ * @old: Size, in bytes, of the old memory block.
+ * @nw: New size, in bytes, of the memory block.
+ *
+ * Return: If nw == old, returns old_double_ptr without changes.
+ * If reallocation fails, returns NULL.
+ */
+char **reallocate_dp(char **old_dp, unsigned int old, unsigned int nw)
+{
+	unsigned int i;
+	char **nwm_dp;
+
+	if (old_dp == NULL)
+	{
+		return (malloc(nw * sizeof(char *)));
+	}
+
+	if (nw == old)
+	{
+		return (old_dp);
+	}
+
+	nwm_dp = malloc(nw * sizeof(char *));
+	if (nwm_dp == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < old)
+	{
+		nwm_dp[i] = old_dp[i];
+		i++;
+	}
+
+	free(old_dp);
+
+	return (nwm_dp);
+}
